fbb2c6f027b830e8e7de57320984682f6afd79f5
Ameer <ameerapex496@gmail.com>
2023-12-05 00:53:56 +0100

    added function to navbar

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
-console.log("hallo world");
+// selecting our elements
+var navbarEl = document.getElementsByClassName("navbar")[0];
+// our functions
+var navDown = function () {
+    // check if window scroll y is higher then navbar height
+    if (window.scrollY > navbarEl.offsetHeight + 50) {
+        navbarEl.classList.add("active");
+    }
+    else {
+        navbarEl.classList.remove("active");
+    }
+};
+// our window eventlisnter
+window.addEventListener("scroll", navDown);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,15 @@
-console.log("hallo world");
+// selecting our elements
+const navbarEl = document.getElementsByClassName("navbar")[0] as any;
+
+// our functions
+const navDown = () => {
+  // check if window scroll y is higher then navbar height
+  if (window.scrollY > navbarEl.offsetHeight + 50) {
+    navbarEl.classList.add("active");
+  } else {
+    navbarEl.classList.remove("active");
+  }
+};
+
+// our window eventlisnter
+window.addEventListener("scroll", navDown);
